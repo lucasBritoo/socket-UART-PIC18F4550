9cfd1bdd2ef3c41f74167ef7e93875a5fd4de7ee
lucasBritoo <britol599@gmail.com>
2021-03-10 04:10:41 -0300

    Adicionando thread para leitura da serial

--- host/serial.c
+++ host/serial.c
@@ -6,6 +6,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include <pthread.h>
+
 int inicializaSerial(){
 	return open("/dev/ttyUSB0", O_RDWR);
 }
@@ -57,29 +59,41 @@ int configuraSerial(int serial_port, struct termios *tty){
 	return 1;
 }
 
-void escreverSerial(int serial_port){
-	char msg[10];
+void * threadLeitura(void *arg){
+	char read_buf[256];
+	int *serial_port = (int *) arg;
+	memset(&read_buf, '\0', sizeof(read_buf));
 
-	strcpy(msg, "OLA MUNDO");
+	while(1){
+		int num_bytes = read(*serial_port, &read_buf, sizeof(read_buf));
+
+		if(num_bytes < 0) {
+			printf("Error reading: %s\n", strerror(errno));
+		}
+		else {
+			printf("Read %i bytes. Received message: %s\n", num_bytes, read_buf);
+			strcpy(read_buf, "");
+			memset(&read_buf, '\0', sizeof(read_buf));
+
+		}
+	}
+}
+
+void escreverSerial(int serial_port){
+	char msg[3];
+
+	strcpy(msg, "AB");
 	write(serial_port, msg, strlen(msg));
 }
 
 void lerSerial(int serial_port){
-	char read_buf[256];
-
-	memset(&read_buf, '\0', sizeof(read_buf));
-
-	int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
-
-	if(num_bytes < 0) {
-		printf("Error reading: %s\n", strerror(errno));
-	}
-	else {
-		printf("Read %i bytes. Received message: %s\n", num_bytes, read_buf);
-	}
+	
 }
 
 int main() {
+	pthread_t thread1;
+	int create, opc = 1;
+	long arg =1;
 	int serial_port = inicializaSerial();					//abre porta serial
 
 	struct termios tty;
@@ -88,13 +102,30 @@ int main() {
 
 	if(connect < 0) {
 		printf("Conexão rejeitada, verifique erro");
+		close(serial_port);
+		return 0;
 	}
 
 	else{
-		escreverSerial(serial_port);
-	
+		create = pthread_create(&thread1, NULL, threadLeitura, (void *) (&serial_port));
+
+		while(opc != 0){
+			printf("-------------------------------\n");
+			printf("1 - Escrever na serial\n");
+			printf("2 - Sair\n");
+			scanf("%d", &opc);
+
+			switch(opc){
+				case 1:
+					escreverSerial(serial_port);
+					break;
+				case 2:
+					printf("Conexão encerrada, fechando o programa...\n");
+					pthread_cancel(thread1);
+					close(serial_port);
+					return 0;
+			}
+		}
 	}
 
-	close(serial_port);
-	return 0;
 }
